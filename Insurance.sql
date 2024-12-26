75702adb04dff84e65421587acaf8fb8024eca0c
Anusha B <169157133+Anusha-B-2005@users.noreply.github.com>
2024-12-26 21:56:49 +0530

    Created Insurance.sql

---
+++ Insurance.sql
@@ -0,0 +1,35 @@
+DROP DATABASE if exists Insurance;
+CREATE DATABASE Insurance;
+USE Insurance;
+
+CREATE TABLE PERSON(
+    driver_id VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    address VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE CAR(
+    regno VARCHAR(10) PRIMARY KEY,
+    model VARCHAR(10) NOT NULL,
+    year INT
+);
+
+CREATE TABLE ACCIDENT(
+    report_number INT PRIMARY KEY,
+    acc_date DATE NOT NULL,
+    location VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE OWNS(
+    driver_id VARCHAR(10) NOT NULL,
+    regno VARCHAR(10) NOT NULL,
+    FOREIGN KEY (driver_id) REFERENCES PERSON (driver_id)
+);
+
+CREATE TABLE PARTICIPATED (
+    driver_id VARCHAR(10),
+    regno VARCHAR(10) NOT NULL,
+    report_number INT NOT NULL,
+    damage_amout INT NOT NULL,
+    FOREIGN KEY (driver_id) REFERENCES PERSON (driver_id)
+);
